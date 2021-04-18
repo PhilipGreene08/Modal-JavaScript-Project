bbcb085b7bcc3cfb10b9e9f40689838c035bc1f6
PhilipGreene08 <54162361+PhilipGreene08@users.noreply.github.com>
2021-04-18 14:08:41 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,6 +6,11 @@
   const lightboxItem = document.querySelector('.lightbox-item')
   const storeImage = document.querySelectorAll('.store-img')
 
+  const btnLeft = document.querySelector('.btnLeft')
+  const btnRight = document.querySelector('.btnRight')
+
+  const closeBox = document.querySelector('.lightbox-close')
+
   //push all item images in an array
   let imgArr = []
   storeImage.forEach(img => {
@@ -15,32 +20,43 @@
   //img counter to iterate through each img
   let imgCounter = 0
 
-  console.log(imgArr);
   //click store item to bring up pics list
   //add click ability to each item in store
   storeItems.forEach(item => {
     item.addEventListener('click', e => {
+      const itemURL = e.target.src
       lightBoxContainer.classList.add(`show`)
-      console.log(item);
-      lightBoxContainer.style.background = `url(${item.src})`
+
+      lightboxItem.style.backgroundImage = `url(${itemURL})`
+      imgCounter = imgArr.indexOf(itemURL); //this is the line of code that took the longest to figure out
+      console.log(imgCounter);
     })
   })
 
-  //add right btn
-
   //add left btn
+  btnLeft.addEventListener('click', e => {
+    imgCounter--
+    if (imgCounter < 0) {
+      //console.log(imgCounter);
+      imgCounter = imgArr.length - 1
+    }
+    console.log(imgCounter);
+    lightboxItem.style.backgroundImage = `url(${imgArr[imgCounter]})`
+  })
+
+  //add right btn
+  btnRight.addEventListener('click', e => {
+    imgCounter++
+    if (imgCounter >= imgArr.length) {
+      imgCounter = 0
+    }
+    lightboxItem.style.backgroundImage = `url(${imgArr[imgCounter]})`
+  })
 
   //close window
+  closeBox.addEventListener('click', e => {
+    lightBoxContainer.classList.remove(`show`)
+    console.log(`yes`);
+  })
 
 })()
-
-/*
-Notes during project
-1. Tried to add an event listener to a node list. Had to actually do a forEach on the node list.
-https://stackoverflow.com/questions/55667793/uncaught-typeerror-input-addeventlistener-is-not-a-function
-2. Tried to use style.classList and then .show instead of just the string show. 'style not needed'
-3. How to access the individual CSS properties (without simply adding a class)
-4. I struggled with change the background. I thought I would be able to simply use element.style.background = 'image' but I realized I needed to use 'url(img);
-5. Had a bug in my code where I misspelled background. backgound vs. background 
-
-*/
